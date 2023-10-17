97a088b1de6a69a2131eaec1164193b29063d3a8
kamil180895 <kamil180895@wp.pl>
2023-10-17 18:50:12 +0200

    Added requsition of player ids for connection purposes

--- mobile/Assets/UnityTransportClientManager.cs
+++ mobile/Assets/UnityTransportClientManager.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
+using System.Text;
 using Unity.Collections;
 using Unity.Networking.Transport;
 using UnityEngine;
+using Newtonsoft.Json;
+using System;
+
+enum MessageEvent
+{
+    GET_PLAYER_ID = 0,
+    SET_PLAYER_ID,
+    BUTTON_PUSHED,
+    JOYSTICK_POSITION,
+    START_MINIGAME,
+    ABORT_MINIGAME,
+    FINISH_MINIGAME_PART,
+}
 
 public class ClientManager : MonoBehaviour
 {
     public NetworkDriver m_Driver;
     public NetworkConnection m_Connection;
     public bool Done;
+    int playerId = -1;
 
 
     void Start()
@@ -45,15 +60,25 @@ public class ClientManager : MonoBehaviour
             {
                 Debug.Log("We are now connected to the server");
 
-                uint value = 1;
-                m_Driver.BeginSend(m_Connection, out var writer);
-                writer.WriteUInt(value);
-                m_Driver.EndSend(writer);
+                if(playerId == -1)
+                {
+                    Dictionary<string, string> dict_message = new Dictionary<string, string>();
+                    dict_message.Add("event", MessageEvent.GET_PLAYER_ID.ToString());
+                    Debug.Log(JsonConvert.SerializeObject(dict_message));
+                    SendMessage(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(dict_message)));
+                }
+                //uint value = 1;
+                //m_Driver.BeginSend(m_Connection, out var writer);
+                //writer.WriteUInt(value);
+                //m_Driver.EndSend(writer);
             }
             else if (cmd == NetworkEvent.Type.Data)
             {
-                uint value = stream.ReadUInt();
-                Debug.Log("Got the value = " + value + " back from the server");
+                Debug.Log("Reading Data...");
+                NativeArray<byte> buffer = new NativeArray<byte>(stream.Length, Allocator.Temp, NativeArrayOptions.ClearMemory);
+                stream.ReadBytes(buffer);
+                HandleMessage(buffer.ToArray());
+                buffer.Dispose();
                 Done = true;
                 m_Connection.Disconnect(m_Driver);
                 m_Connection = default(NetworkConnection);
@@ -66,4 +91,30 @@ public class ClientManager : MonoBehaviour
         }
     }
 
+    void HandleMessage(byte[] message)
+    {
+        Debug.Log(Encoding.UTF8.GetString(message));
+
+        Dictionary<string, string> dict_message = JsonConvert.DeserializeObject<Dictionary<string, string>>(Encoding.UTF8.GetString(message));
+        if (dict_message["event"].Equals(MessageEvent.SET_PLAYER_ID.ToString()))
+        {
+            playerId = Int32.Parse(dict_message["player"]);
+            Debug.Log($"Player ID: {playerId}");
+        }
+        else
+        {
+            //handle invalid message
+        }
+        
+    }
+
+    void SendMessage(byte[] message)
+    {
+        Debug.Log("Sending data...");
+        NativeArray<byte> buffer = new NativeArray<byte>(message, Allocator.Temp);
+        m_Driver.BeginSend(NetworkPipeline.Null, m_Connection, out var writer);
+        writer.WriteBytes(buffer);
+        m_Driver.EndSend(writer);
+        buffer.Dispose();
+    }
 }
--- pc/Assets/UnityTransportServerManager.cs
+++ pc/Assets/UnityTransportServerManager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Assertions;
 
 using Unity.Collections;
@@ -6,12 +6,25 @@ using Unity.Networking.Transport;
 using System.Text;
 using System.IO;
 using Unity.VisualScripting;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+enum MessageEvent
+{
+    GET_PLAYER_ID = 0,
+    SET_PLAYER_ID,
+    BUTTON_PUSHED,
+    JOYSTICK_POSITION,
+    START_MINIGAME,
+    ABORT_MINIGAME,
+    FINISH_MINIGAME_PART,
+}
 
 public class ServerManager : MonoBehaviour
 {
     public NetworkDriver m_Driver;
     private NativeList<NetworkConnection> m_Connections;
-
+    int maxPlayerId = 0;
 
     void Start()
     {
@@ -67,13 +80,11 @@ public class ServerManager : MonoBehaviour
             {
                 if (cmd == NetworkEvent.Type.Data)
                 {
+                    Debug.Log("Reading data...");
                     NativeArray<byte> buffer = new NativeArray<byte>(stream.Length, Allocator.Temp, NativeArrayOptions.ClearMemory);
                     stream.ReadBytes(buffer);
-                    HandleMessages(buffer.ToArray());
+                    HandleMessage(buffer.ToArray());
                     buffer.Dispose();
-                    //m_Driver.BeginSend(NetworkPipeline.Null, m_Connections[i], out var writer);
-                    //writer.WriteUInt(number);
-                    //m_Driver.EndSend(writer);
                 }
                 else if (cmd == NetworkEvent.Type.Disconnect)
                 {
@@ -84,15 +95,36 @@ public class ServerManager : MonoBehaviour
         }
     }
 
-    void HandleMessages(byte[] message)
+    void HandleMessage(byte[] message)
     {
         Debug.Log(Encoding.UTF8.GetString(message));
-        SendMessage(Encoding.UTF8.GetBytes("Hello Client!"), 0);
+        
+        Dictionary<string, string> dict_message = JsonConvert.DeserializeObject<Dictionary<string, string>>(Encoding.UTF8.GetString(message));
+        if (dict_message["event"].Equals(MessageEvent.GET_PLAYER_ID.ToString()))
+        {
+            Dictionary<string, string> dict_response = new Dictionary<string, string>();
+            dict_response.Add("event", MessageEvent.SET_PLAYER_ID.ToString());
+            dict_response.Add("player", maxPlayerId++.ToString());
+            SendMessage(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(dict_response)), maxPlayerId - 1);
+        }
+        else if (dict_message["event"].Equals(MessageEvent.BUTTON_PUSHED))
+        {
+            //handle using dict_response["parameter"]
+        }
+        else if (dict_message["event"].Equals(MessageEvent.JOYSTICK_POSITION))
+        {
+            //handle using dict_response["parameter_x"] i dict_response["parameter_y"]
+        }
+        else
+        {
+            //handle invalid message
+        }
     }
 
-    //TO DO: lepsza translacja playerId do connection
+    //TO DO: lepsza translacja playerId do connection, bo obecna może się rozjechać przy problemach z połączeniem
     void SendMessage(byte[] message, int playerId)
     {
+        Debug.Log("Sending data...");
         NativeArray<byte> buffer = new NativeArray<byte>(message, Allocator.Temp);
         m_Driver.BeginSend(NetworkPipeline.Null, m_Connections[playerId], out var writer);
         writer.WriteBytes(buffer);
