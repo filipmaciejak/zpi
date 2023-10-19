4f84f9220531bbbb0b13aae1aa122143d2d444ad
qriaa <49167319+qriaa@users.noreply.github.com>
2023-10-19 21:21:30 +0200

    Add message event handling callbacks
    Add GameManager
    
    Make MovementScene send controls data

---
+++ mobile/Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+enum MinigameType
+{
+    MOVEMENT_MODULE
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public ClientManager clientManager;
+    
+    // This should be grabbed by a new section instantiated by the game manager
+    // before the first update.
+    [NonSerialized]
+    public Dictionary<string, string> MinigameInitializationData;
+    public event Action<Dictionary<string, string>> UpdateMinigameData;
+    
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this);
+        else
+            Instance = this;
+        DontDestroyOnLoad(gameObject);
+        clientManager = GetComponentInChildren<ClientManager>();
+    }
+
+    private void OnEnable()
+    {
+        clientManager.StartMinigame += OnStartMinigame;
+        clientManager.UpdateMinigame += OnUpdateMinigame;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnDisable()
+    {
+        clientManager.StartMinigame -= OnStartMinigame;
+        clientManager.UpdateMinigame -= OnUpdateMinigame;
+    }
+
+    public void OnStartMinigame(Dictionary<string,string> data)
+    {
+        Boolean conversionSuccess = Enum.TryParse(data["minigame"], out MinigameType minigame);
+        if (!conversionSuccess)
+        {
+            Debug.Log("This minigame does not exist!: " + data["minigame"]);
+            return;
+        }
+        
+        MinigameInitializationData = data;
+        SceneManager.LoadScene(data["minigame"]);
+    }
+
+    public void OnUpdateMinigame(Dictionary<string, string> data)
+    {
+        UpdateMinigameData?.Invoke(data);
+    }
+}
