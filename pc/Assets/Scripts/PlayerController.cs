25eef0f3c790760d0cc754eca49fd434d4fa76ba
filipmaciejak <fimaciejak@gmail.com>
2023-10-25 11:57:31 +0200

    event manager added

--- pc/Assets/Scripts/PlayerController.cs
+++ pc/Assets/Scripts/PlayerController.cs
@@ -5,25 +5,22 @@ using UnityEngine;
 // This class is only here temporarily to test interactions without mobile controls
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] Player player;
+    [SerializeField] private int _id = 0;
 
     void Update()
     {
-        player.UpdateMoveInput(Input.GetAxisRaw("Horizontal"));
+        CrewmateEventManager.instance.onCrewmateMoveInputUpdate.Invoke(_id, Input.GetAxisRaw("Horizontal"), 0f);
         if (Input.GetButtonDown("Jump"))
         {
-            player.Jump();
+            CrewmateEventManager.instance.onCrewmateJump.Invoke(_id);
         }
         if (Input.GetButtonDown("Fire1"))
         {
-            if (!player.usedModule)
-            {
-                player.Interact();
-            }
-            else
-            {
-                player.FinishInteraction();
-            }
+            CrewmateEventManager.instance.onCrewmateInteractionStart.Invoke(_id);
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            CrewmateEventManager.instance.onCrewmateInteractionEnd.Invoke(_id);
         }
     }
 }
