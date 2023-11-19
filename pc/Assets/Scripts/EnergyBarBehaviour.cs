12a390ad1abf700132ea88edf8b2ee7a5e8110d7
LunaticZebra <maciek.madry@o2.pl>
2023-11-19 09:39:25 +0100

    Add battery icon

---
+++ pc/Assets/Scripts/EnergyBarBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyBarBehaviour : MonoBehaviour
+{
+    public List<GameObject> energyBars;
+
+    private int energyLevel = 4;
+
+    void Update()
+    {
+        
+    }
+
+    public void EnergyLevelChanged()
+    {
+
+    }
+}
