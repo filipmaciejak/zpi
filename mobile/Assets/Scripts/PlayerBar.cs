95a21b609f0f991d5659694b0573049b31b7597f
qriaa <49167319+qriaa@users.noreply.github.com>
2023-11-19 17:02:28 +0100

    Add player bar indicating player color

---
+++ mobile/Assets/Scripts/PlayerBar.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBar : MonoBehaviour
+{
+    public Color noPlayerColor;
+    public Color playerOneColor;
+    public Color playerTwoColor;
+    public Color playerThreeColor;
+    public Color playerFourColor;
+
+    private Dictionary<int, Color> _playerColorMapping;
+    
+    private ClientManager _clientManager;
+    private Image _barImage;
+    
+    public void Awake()
+    {
+        _clientManager = GameManager.Instance.clientManager;
+        _barImage = GetComponent<Image>();
+        
+        // an unfortunate way to serialize colors in the unity editor
+        _playerColorMapping = new Dictionary<int, Color>
+        {
+            { -1, noPlayerColor },
+            { 0, playerOneColor },
+            { 1, playerTwoColor },
+            { 2, playerThreeColor },
+            { 3, playerFourColor }
+        };
+    }
+
+    public void Start()
+    {
+        _barImage.color = _playerColorMapping[_clientManager.playerId];
+    }
+}
