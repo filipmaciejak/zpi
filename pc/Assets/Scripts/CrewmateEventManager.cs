29110bc5ef00773c25819cb0f4781bae8aa6f20c
filipmaciejak <fimaciejak@gmail.com>
2023-11-23 17:39:33 +0100

    refactor crewmate event

--- pc/Assets/Scripts/CrewmateEventManager.cs
+++ pc/Assets/Scripts/CrewmateEventManager.cs
@@ -7,45 +7,50 @@ public class CrewmateEventManager : MonoBehaviour
 {
     public static CrewmateEventManager instance;
 
-    public UnityEvent<int /* crewmate id */, float /* x */, float /* y */> onCrewmateMoveInputUpdate;
-    public UnityEvent<int /* crewmate id */> onCrewmateButtonAPushed;
-    public UnityEvent<int /* crewmate id */> onCrewmateButtonAReleased;
-    public UnityEvent<int /* crewmate id */> onCrewmateButtonBPushed;
-    public UnityEvent<int /* crewmate id */> onCrewmateButtonBReleased;
+    public UnityEvent<int, float /* x */, float /* y */> onCrewmateMoveInputUpdate;
+    public UnityEvent<int> onCrewmateButtonAPushed;
+    public UnityEvent<int> onCrewmateButtonAReleased;
+    public UnityEvent<int> onCrewmateButtonBPushed;
+    public UnityEvent<int> onCrewmateButtonBReleased;
 
     void Awake()
     {
         DontDestroyOnLoad(this);
         instance = this;
+        
+        onCrewmateMoveInputUpdate = new UnityEvent<int, float, float>();
+        onCrewmateButtonAPushed = new UnityEvent<int>();
+        onCrewmateButtonAReleased = new UnityEvent<int>();
+        onCrewmateButtonBPushed = new UnityEvent<int>();
+        onCrewmateButtonBReleased = new UnityEvent<int>();
     }
 
     void Start()
     {
-        onCrewmateMoveInputUpdate = new UnityEvent<int, float, float>();
         onCrewmateMoveInputUpdate.AddListener(
             (id, inputX, inputY) => {
                 Debug.Log("CrewmateEventManager: onCrewmateMoveInputUpdate<" + id + ">");
             }
         );
-        onCrewmateButtonAPushed = new UnityEvent<int>();
+
         onCrewmateButtonAPushed.AddListener(
             (id) => {
                 Debug.Log("CrewmateEventManager: onCrewmateButtonAPushed<" + id + ">");
             }
         );
-        onCrewmateButtonAReleased = new UnityEvent<int>();
+
         onCrewmateButtonAReleased.AddListener(
             (id) => {
                 Debug.Log("CrewmateEventManager: onCrewmateButtonAReleased<" + id + ">");
             }
         );
-        onCrewmateButtonBPushed = new UnityEvent<int>();
+
         onCrewmateButtonBPushed.AddListener(
             (id) => {
                 Debug.Log("CrewmateEventManager: onCrewmateButtonBPushed<" + id + ">");
             }
         );
-        onCrewmateButtonBReleased = new UnityEvent<int>();
+        
         onCrewmateButtonBReleased.AddListener(
             (id) => {
                 Debug.Log("CrewmateEventManager: onCrewmateButtonBReleased<" + id + ">");
