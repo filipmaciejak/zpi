f7a137390b8bd5797ee01ae89273f830f294019e
filipmaciejak <fimaciejak@gmail.com>
2023-12-12 22:09:00 +0100

    main ladder functionality

--- pc/Assets/Scripts/CrewmateEventManager.cs
+++ pc/Assets/Scripts/CrewmateEventManager.cs
@@ -29,7 +29,7 @@ public class CrewmateEventManager : MonoBehaviour
     {
         onCrewmateMoveInputUpdate.AddListener(
             (id, inputX, inputY) => {
-                Debug.Log("CrewmateEventManager: onCrewmateMoveInputUpdate<" + id + ", " + inputX + ", " + inputY + ">");
+                Debug.Log("CrewmateEventManager: onCrewmateMoveInputUpdate<" + id + ", ...>");
             }
         );
 
